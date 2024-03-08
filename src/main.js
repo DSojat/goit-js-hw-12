1a64368261812713366432f5efe6dd03c94d9dfa
DSojat <145054801+DSojat@users.noreply.github.com>
2024-03-08 16:21:59 +0200

    fix icon

--- src/main.js
+++ src/main.js
@@ -75,7 +75,7 @@ searchForm.addEventListener('submit', event => {
           message:
             'Sorry, there are no images matching your search query. Please try again!',
           backgroundColor: '#EF4040',
-          iconUrl: './img/x-octagon.svg',
+          iconUrl: './img/xoctagon.svg',
         });
       } else {
         renderGallery(hits);
